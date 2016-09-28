1a675e51f31a6928a67726b1c370060e565fa6d4
Calvin Vuong <cvuong@stuy.edu>
2016-09-28 14:44:34 -0400

    str_functions.c

--- str_functions.c
+++ str_functions.c
@@ -13,7 +13,7 @@ int strlen(char *str) {
 
 // copies n characters of string src onto dest
 // returns pointer to the destination string
-char *strncpy(char *dest, char *src, int n) {
+char * strncpy(char *dest, char *src, int n) {
   char *dest_head = dest;
   while ( *src != 0 && n != 0 ) { // loop until you reach terminator of source or n is reached
     *dest = *src;
@@ -25,10 +25,23 @@ char *strncpy(char *dest, char *src, int n) {
   *dest = 0;
   return dest_head;
 }
-  
+
+// returns > 0 if s1 is greater than s2
+// returns < 0 if s1 is less than s2
+// returns 0 if s1 == s2
+int strcmp( char *s1, char *s2 ) {
+  while ( *s1 != 0 || *s2 != 0 ) {
+    if ( *s1 != *s2 )
+      return *s1 - *s2;
+    s1++;
+    s2++;
+  }
+  return 0;
+}
+
 // returns memory address of char c in string s
 // returns NULL if c not in s
-char *strchr(char *s, char c) {
+char * strchr(char *s, char c) {
   while ( *s != c ) {
     if ( *s == 0 ) // null terminator
       return NULL;
@@ -58,10 +71,23 @@ void test_strchr() {
   printf( "Memory address of l in hello: %d\n", strchr(hello, 'A') ); // should be 0 
 }
 
+void test_strcmp() {
+  char a[4] = "cat";
+  char b[4] = "cap";
+  char c[12] = "catastrophe";
+  printf( "strcmp(\"cat\", \"cap\"): %d\n", strcmp(a,b) );
+  printf( "strcmp(\"cap\", \"cat\"): %d\n", strcmp(b,a) );
+
+  printf( "strcmp(\"cat\", \"catastrophe\"): %d\n", strcmp(a,c) );
+  printf( "strcmp(\"catastrophe\", \"cat\"): %d\n", strcmp(c,a) );
+}
+
 int main() {
   test_strlen();
   test_strncpy();
+  test_strcmp();
   test_strchr();
+
   return 0;
 }
 	  
